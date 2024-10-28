0cf2be48d3c65114e73fe4c58c73be1baddd4cbd
Khang Ho <khangho150@gmail.com>
2024-10-28 21:39:54 +0700

    Thêm fragment staff, profile

--- app/src/main/java/com/example/studentmanagement/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/studentmanagement/fragments/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.studentmanagement.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,18 +8,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.studentmanagement.R;
+import com.example.studentmanagement.activities.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -26,19 +25,13 @@ public class ProfileFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private Button logoutButton;
+    private TextView usernameText;
+
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
@@ -60,7 +53,24 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        logoutButton = view.findViewById(R.id.logout_button);
+        usernameText = view.findViewById(R.id.text_username);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        usernameText.setText(user.getEmail());
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        return view;// Inflate the layout for this fragment
     }
 }
