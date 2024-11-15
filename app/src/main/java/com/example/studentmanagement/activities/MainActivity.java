df18caa4c5ddae6b41d7b773612efe093b8f693a
KJuly28th <kiet.tk1508@gmail.com>
2024-11-15 16:13:12 +0700

    Add files via upload

--- app/src/main/java/com/example/studentmanagement/activities/MainActivity.java
+++ app/src/main/java/com/example/studentmanagement/activities/MainActivity.java
@@ -7,7 +7,9 @@ import android.widget.Toast;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.studentmanagement.R;
@@ -15,7 +17,13 @@ import com.example.studentmanagement.adapters.ViewPagerAdapter;
 import com.example.studentmanagement.data.Student;
 import com.example.studentmanagement.fragments.StudentListFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
 
 import java.util.List;
 
@@ -25,6 +33,9 @@ public class MainActivity extends FragmentActivity {
     private ActivityResultLauncher<Intent> addStudentLauncher;
     private boolean isFirstTime = true;
     private FirebaseFirestore db;
+    private DatabaseReference userRef;
+    private String userRole;
+    private ListenerRegistration roleListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,48 +45,6 @@ public class MainActivity extends FragmentActivity {
         db = FirebaseFirestore.getInstance();
         viewPager = findViewById(R.id.viewPager);
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        // Khởi tạo ViewPagerAdapter
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), getLifecycle());
-        viewPager.setAdapter(adapter);
-
-        // Đăng ký sự kiện khi trang được thay đổi trong ViewPager
-        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                if (position == 0) {
-                    bottomNavigationView.setSelectedItemId(R.id.item_home);
-                } else if (position == 1) {
-                    bottomNavigationView.setSelectedItemId(R.id.item_student);
-                } else if (position == 2) {
-
-                } else if (position == 3) {
-                    bottomNavigationView.setSelectedItemId(R.id.item_staff);
-                } else if (position == 4) {
-                    bottomNavigationView.setSelectedItemId(R.id.item_profile);
-                }
-            }
-        });
-
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            if (item.getItemId() == R.id.item_profile) {
-                viewPager.setCurrentItem(4);
-            }
-            if (item.getItemId() == R.id.item_staff) {
-                viewPager.setCurrentItem(3);
-            }
-            if (item.getItemId() == R.id.item_student) {
-                viewPager.setCurrentItem(1);
-            }
-            if (item.getItemId() == R.id.item_home) {
-                viewPager.setCurrentItem(0);
-            }
-            if (item.getItemId() == R.id.item_add) {
-                showAddDialog();
-            }
-            return true;
-        });
 
         // Khởi tạo launcher để nhận kết quả từ AddStudentActivity
         addStudentLauncher = registerForActivityResult(
@@ -129,26 +98,136 @@ public class MainActivity extends FragmentActivity {
                 }
         );
 
-        // Mở trang mặc định và chọn mục Student nếu lần đầu vào
-        viewPager.setCurrentItem(0);
-        bottomNavigationView.setSelectedItemId(R.id.item_home);
+        checkUserRole();
+    }
+
+    private void checkUserRole() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            userRef = FirebaseDatabase.getInstance().getReference("users")
+                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+            userRef.get().addOnSuccessListener(dataSnapshot -> {
+                if (dataSnapshot.exists()) {
+                    String role = dataSnapshot.child("role").getValue(String.class);
+                    if ("Employee".equals(role)) {
+                        bottomNavigationView.getMenu().findItem(R.id.item_add).setVisible(false);
+                        bottomNavigationView.getMenu().findItem(R.id.item_staff).setVisible(false);
+
+                    }
+                    if ("Manager".equals(role)) {
+                        bottomNavigationView.getMenu().findItem(R.id.item_staff).setVisible(false);
+                    }
+                    setupViewPager(role);
+                    setupBottomNavigation(role);
+                }
+            });
+        }
+    }
+
+    private void setupViewPager(String role) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), getLifecycle(), role);
+        viewPager.setAdapter(adapter);
+
+        // Đăng ký sự kiện khi trang được thay đổi trong ViewPager
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                if (position == 0) {
+                    bottomNavigationView.setSelectedItemId(R.id.item_home);
+                } else if (position == 1) {
+                    bottomNavigationView.setSelectedItemId(R.id.item_student);
+                } else if (position == 2) {
+
+                } else if (position == 3) {
+                    bottomNavigationView.setSelectedItemId(R.id.item_staff);
+                } else if (position == 4) {
+                    bottomNavigationView.setSelectedItemId(R.id.item_profile);
+                }
+            }
+        });
     }
 
     private void showAddDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Bạn muốn thêm");
-        String[] options = {"Sinh Viên", "Nhân Viên"};
-        builder.setItems(options, (dialog, which) -> {
-            if (which == 0) {
-                // Chọn Sinh Viên -> Mở AddStudentActivity
-                Intent intent = new Intent(this, AddStudentActivity.class);
-                addStudentLauncher.launch(intent);
-            } else {
-                // Chọn Nhân Viên -> Mở AddStaffActivity
-                Intent intent = new Intent(this, AddStaffActivity.class);
-                startActivity(intent);
+        userRef.get().addOnSuccessListener(dataSnapshot -> {
+            if (dataSnapshot.exists()) {
+                String role = dataSnapshot.child("role").getValue(String.class);
+                if ("Manager".equals(role)) {
+                    String[] options = {"Sinh Viên"};
+                    builder.setItems(options, (dialog, which) -> {
+                        if (which == 0) {
+                            // Chọn Sinh Viên -> Mở AddStudentActivity
+                            Intent intent = new Intent(this, AddStudentActivity.class);
+                            addStudentLauncher.launch(intent);
+                        }
+                    });
+                    builder.show();
+                }
+                else{
+                    String[] options = {"Sinh Viên", "Nhân Viên"};
+                    builder.setItems(options, (dialog, which) -> {
+                        if (which == 0) {
+                            // Chọn Sinh Viên -> Mở AddStudentActivity
+                            Intent intent = new Intent(this, AddStudentActivity.class);
+                            addStudentLauncher.launch(intent);
+                        } else {
+                            // Chọn Nhân Viên -> Mở AddStaffActivity
+                            Intent intent = new Intent(this, AddStaffActivity.class);
+                            startActivity(intent);
+                        }
+                    });
+                    builder.show();
+                }
             }
         });
-        builder.show();
+
+    }
+    private void setupBottomNavigation(String role) {
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if ("Employee".equals(role)){
+                if (item.getItemId() == R.id.item_home) {
+                    viewPager.setCurrentItem(0);
+                }
+                if (item.getItemId() == R.id.item_student) {
+                    viewPager.setCurrentItem(1);
+                }
+                if (item.getItemId() == R.id.item_profile) {
+                    viewPager.setCurrentItem(2);
+                }
+            } else if ("Manager".equals(role)){
+                if (item.getItemId() == R.id.item_home) {
+                    viewPager.setCurrentItem(0);
+                }
+                if (item.getItemId() == R.id.item_student) {
+                    viewPager.setCurrentItem(1);
+                }
+                if (item.getItemId() == R.id.item_add) {
+                    showAddDialog();
+                }
+                if (item.getItemId() == R.id.item_profile) {
+                    viewPager.setCurrentItem(3);
+                }
+            } else {
+                if (item.getItemId() == R.id.item_home) {
+                    viewPager.setCurrentItem(0);
+                }
+                if (item.getItemId() == R.id.item_student) {
+                    viewPager.setCurrentItem(1);
+                }
+                if (item.getItemId() == R.id.item_add) {
+                    showAddDialog();
+                }
+                if (item.getItemId() == R.id.item_staff) {
+                    viewPager.setCurrentItem(3);
+                }
+                if (item.getItemId() == R.id.item_profile) {
+                    viewPager.setCurrentItem(4);
+                }
+            }
+            return true;
+        });
     }
 }
